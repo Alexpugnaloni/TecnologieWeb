37be47214e83a03faaf28c59bd460e84d0ed616d
Asus <Asus@LAPTOP-RS64K0LE.home>
2022-05-18 01:22:34 +0200

    filtri

--- resources/views/auth/filtri.blade.php
+++ resources/views/auth/filtri.blade.php
@@ -1,8 +1,20 @@
-<?php
+@extends('layout.public')
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+@section('title', 'Filtri')
 
+@section('content')
+        
+<div class="static">
+    <h2><center>Filtri</center></h2>
+    
+    <div class="container-contact">
+<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+  <label class="btn btn-outline-primary" for="btnradio1">Appartamento</label>
 
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+  <label class="btn btn-outline-primary" for="btnradio2">Stanza singola</label>
+
+  <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+  <label class="btn btn-outline-primary" for="btnradio3">Stanza doppia</label>
+</div>
